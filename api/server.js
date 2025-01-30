b601f4206960227d2687a3dd394e564c2822283c
Umair Ahmed <umairjdu@gmail.com>
2025-01-30 15:08:38 +0500

    remove running server

--- api/server.js
+++ api/server.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 3000;
 // database
 const connectDB = require("../config/db");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-  connectDB();
-});
+connectDB();
+
+module.exports = app
