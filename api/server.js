7cccd5e0cab35f4be580918d77c4460560c05554
Umair Ahmed <umairjdu@gmail.com>
2025-02-20 12:21:54 +0500

    add listener

--- api/server.js
+++ api/server.js
@@ -5,4 +5,9 @@ const connectDB = require("../config/db");
 
 connectDB();
 
+/* Server */
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port", process.env.PORT);
+});
+
 module.exports = app;
