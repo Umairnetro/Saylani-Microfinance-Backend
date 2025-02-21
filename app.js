fdf1b3302d928ed622e0bd3c7e7285d588907795
Umair Ahmed <umairjdu@gmail.com>
2025-02-21 17:00:04 +0500

    cors issue solve

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
 const routes = require("./routes");
 
