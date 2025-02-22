93a66dbe813598bdf15d0eb9d3f1548486fd3ae9
Umair Ahmed <umairjdu@gmail.com>
2025-02-22 14:41:22 +0500

    update

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -1,8 +1,7 @@
 const joi = require("joi");
 
 const userSchema = joi.object({
-  name: joi.string().min(3).max(30).required()
-  .messages({
+  name: joi.string().min(3).max(30).required().messages({
     "string.empty": `Name is required.`,
     "string.min": `Name should have a minimum length of {#limit}.`,
     "string.max": `Name should have a maximum length of {#limit}.`,
@@ -12,10 +11,13 @@ const userSchema = joi.object({
     .pattern(/^\d{13}$/)
     .required()
     .messages({
-      "string.pattern.base":
-        `CNIC must be exactly 13 digits and contain only numbers.`,
+      "string.empty": `CNIC is required.`,
+      "string.pattern.base": `CNIC must be exactly 13 digits and contain only numbers.`,
     }),
-  email: joi.string().email().required(),
+  email: joi.string().email().required().messages({
+    "string.empty": `Email is required.`,
+    "string.email": `Email must be a valid email address.`,
+  }),
 });
 
 const loginSchema = joi.object({
@@ -23,4 +25,4 @@ const loginSchema = joi.object({
   password: joi.string().required(),
 });
 
-module.exports = {userSchema, loginSchema};
+module.exports = { userSchema, loginSchema };
